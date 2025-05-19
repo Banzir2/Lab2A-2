7f6b1ddde975e6d86226129cf8fba2d81e699f33
Banzir2 <eitan.vichik.46@gmail.com>
2025-05-19 16:01:49 +0300

    Mui importante

---
+++ Week2/get_dirs.py
@@ -0,0 +1,9 @@
+import os
+
+if __name__ == '__main__':
+    for directory in ['PartA', 'PartB', 'PartB-LowPass']:
+        lst = []
+        for file in os.listdir(directory):
+            if os.path.isdir(directory + '/' + file):
+                lst.append(file)
+        print(lst)
